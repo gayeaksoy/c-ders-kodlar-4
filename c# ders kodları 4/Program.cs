609a31818e76c9203f2eaae35d308b8bc0648fdd
Hp <Hp@DESKTOP-SAT6TRI>
2025-10-22 13:51:43 +0300

    Proje dosyası ekle.

---
+++ c# ders kodları 4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Fig. 3.11: Welcome3.cs
+// Displaying multiple lines with a single statement.
+using System;
+
+
+class Welcome3
+ {
+ // Main method begins execution of C# app
+static void Main()
+ {
+  Console.WriteLine("Welcome\nto\nC#\nProgramming!");
+  } // end Main
+  } // end class Welcome3
